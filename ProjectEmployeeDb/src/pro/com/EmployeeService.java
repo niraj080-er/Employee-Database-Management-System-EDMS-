60bf4e0e9690c59945dbaee9437f3e4cdcbe3526
Niraj Kumar <niranjan888k@gmail.com>
2023-12-23 23:23:52 +0530

    Add files via upload

---
+++ ProjectEmployeeDb/src/pro/com/EmployeeService.java
@@ -0,0 +1,10 @@
+package pro.com;
+
+public interface EmployeeService {
+	void createEmployeeDb(int size);
+	void availableIndex();
+	void addEmployee(int size);
+	void displayAllEmp();
+	void getEmployeeEid(int eid);
+	void getEmployeeDetailsByDesg(String desgn);
+}
